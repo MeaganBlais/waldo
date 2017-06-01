542f1bd901310d69ee463c963d572bfa1598b072
Meagan Blais <meaganblais@gmail.com>
2017-06-01 13:13:35 -0400

    functioning code, needs refactoring

--- waldo-arr.js
+++ waldo-arr.js
@@ -1,14 +1,12 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found();   // execute callback
-    }
-  }
+    arr.forEach(found);
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+function actionWhenFound(item, index) {
+    if (item === "Waldo") {
+    console.log("Found him!");
+    }
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
